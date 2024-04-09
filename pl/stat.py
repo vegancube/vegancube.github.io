985d5053e237fb4bc75f5fffb20ca5163d3bff86
Michaś Varantsou <meequz@gmail.com>
2024-04-09 14:11:29 +0200

    Add pl stat

---
+++ pl/stat.py
@@ -0,0 +1,26 @@
+from .base import *
+
+
+html_title = 'Global statistics'
+cards_label = html_title
+card_height = 210
+cards = (
+    {'title': 'Jaki ślad węglowy pozostawia produkcja żywności?',
+     'subtitle': 'agronomist.pl',
+     'link': 'https://agronomist.pl/artykuly/jaki-slad-weglowy-pozostawia-produkcja-zywnosci',
+     'preview': '../templates/assets/sliad.jpg',
+     'description': "Produkcja żywności odpowiada za około jedną czwartą łącznej emisji gazów cieplarnianych do atmosfery, przy czym działania związane z hodowlą zwierząt stanowią ok. 18% (co daje 80% ogółu emisji z rolnictwa).",
+    },
+    {'title': 'Rybackie sieci widma, które zostają zgubione w oceanach tylko w ciągu roku, mogą opleść Ziemię 18 razy',
+     'subtitle': 'national-geographic.pl',
+     'link': 'https://www.national-geographic.pl/artykul/rybackie-sieci-widma-ktore-zostaja-zgubione-w-oceanach-tylko-w-ciagu-roku-moga-oplesc-ziemie-18-razy-221024123834',
+     'preview': '../templates/assets/sieci.jpg',
+     'description': "Każdego roku rybacy na całym świecie gubią różnego rodzaju narzędzia połowowe. Utracone przypadkiem lub porzucone sieci stają się pułapkami, śmiertelnie niebezpiecznymi dla morskich zwierząt. Naukowcy policzyli, że w ciągu roku w oceanach zostaje 740 tys. km samych lin.",
+    },
+    {'title': '71% lasów deszczowych ginie na potrzeby przemysłu mięsnego',
+     'subtitle': 'blog.viva.org.pl',
+     'link': 'https://blog.viva.org.pl/2016/11/08/71-lasow-deszczowych-ginie-na-potrzeby-przemyslu-miesnego/',
+     'preview': '../templates/assets/liasy.jpg',
+     'description': "Nowe badania przeprowadzone przez Union of Concerned Scientists zwracają uwagę na związek postępującej deforestacji lasów amazońskich z produkcją wołowiny i rozwojem branży mięsnej.",
+    },
+)
