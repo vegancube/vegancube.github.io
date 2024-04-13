e4d1f8a0627d71f9153b114106e111ad1e45868e
Michaś Varantsou <meequz@gmail.com>
2024-04-13 23:36:15 +0200

    Ru stab

---
+++ ru/index.py
@@ -0,0 +1,28 @@
+from .base import *
+
+
+landheader = {
+    'title': 'Доминион',
+    'description': 'Съёмки с дронов, скрытых и ручных камер обнажают темную подноготную современного животноводства, ставя под сомнение моральность господства человечества над животным миром. Хотя в основном фильм посвящен животным, используемым в пищу, в нем также показано их использование для изготовления одежды, развлечений и исследований.',
+    'btnmain': {
+        'label': 'Смотреть на Youtube',
+        'link': 'https://www.youtube.com/watch?v=BCbNxKvLglQ',
+        'target': '_blank',
+    },
+    # 'btn2': {
+    #     'label': 'Більше фільмів',
+    #     'link': '/uk/films.html',
+    #     'target': '_self',
+    # },
+    'image': {
+        'src': '../templates/assets/dominion.jpg',
+        'link': 'https://www.youtube.com/watch?v=BCbNxKvLglQ',
+        'target': '_blank',
+    },
+}
+
+contact = {
+    'title': 'Контакт',
+    'follow_us': 'Подписывайтесь на нас',
+    'donations': 'Донаты',
+}
