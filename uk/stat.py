9e386cafe23a09691e1230059f1d005d016d01f2
Michaś Varantsou <meequz@gmail.com>
2024-04-16 17:24:59 +0200

    Uk stat

--- uk/stat.py
+++ uk/stat.py
@@ -1,11 +1,26 @@
 from .base import *
 
 
-stub = {
-    'title': 'Ще не готово',
-    'text': "Ця сторінка поки що не готова. Ми працюємо над сайтом, і вона скоро з'явиться. Можете скористатися цією сторінкою іншими мовами.",
-    'buttons': [
-        {'label': 'Англійська версія', 'link': '/en/stat.html'},
-        {'label': 'Польська версія', 'link': '/pl/stat.html'},
-    ],
-}
+html_title = 'Глобальна статистика'
+cards_label = html_title
+card_height = 210
+cards = (
+    {'title': "На виробництво м’яса припадає майже 60% усіх парникових газів від виробництва харчових продуктів",
+     'subtitle': 'Співчуття у фермерстві',
+     'link': 'https://ciwf.in.ua/?p=3698',
+     'preview': 'https://ciwf.in.ua/wp-content/uploads/2022/12/%D0%BE%D0%B1%D0%BA%D0%BB%D0%B0%D0%B4%D0%B8%D0%BD%D0%BA%D0%B0-%D1%81%D1%82%D0%B0%D1%82%D1%82%D1%8F.png',
+     'description': "У цій статті активістки кампанії «UA Plant-based» розповідають про нове велике дослідження, яке показало, що виробництво м’яса в усьому світі вдвічі більше забруднює навколишнє середовище, ніж виробництво продуктів рослинного походження.",
+    },
+    {'title': "Половину пластику в океанах продукують рибальські судна — дослідження",
+     'subtitle': 'Порти України',
+     'link': 'https://ports.ua/polovinu-plastiku-v-okeanah-produkuyut-ribalski-sudna-doslidzhennya/',
+     'preview': 'https://ports.ua/wp-content/uploads/2020/12/plastik-ekologiya-1100x450.jpg',
+     'description': "Половина всього пластикового сміття у Світовому океані потрапила туди з 4,5 млн рибальських суден, що ведуть промисел у міжнародних водах та у територіальних водах країн. Такі дані оприлюднено за підсумками Конференції ООН з океану.",
+    },
+    {'title': "Основні причини вирубки лісів",
+     'subtitle': 'Forest Protection Council',
+     'link': 'https://www.forestprotection.com/uk/bilgiler/ormansizlasmanin-baslica-nedenleri/',
+     'preview': 'https://www.forestprotection.com/images/ormansizlasmanin-baslica-nedenleri.jpg',
+     'description': "За останнє століття лісовий покрив у всьому світі був значно скомпрометований, і зелений покрив впав майже до 30 відсотків за всю історію. За даними Продовольчої та сільськогосподарської організації ООН, щороку втрачається приблизно 18 мільйонів акрів лісу.",
+    },
+)
