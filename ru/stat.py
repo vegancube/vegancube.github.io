bd9c35e49af2d48609f8ccffd03e2ede0d70eba2
Michaś Varantsou <meequz@gmail.com>
2024-04-16 16:53:58 +0200

    Ru stat

--- ru/stat.py
+++ ru/stat.py
@@ -1,11 +1,38 @@
 from .base import *
 
 
-stub = {
-    'title': 'Еще не готово',
-    'text': "Эта страница еще не готова. Мы работаем над сайтом, и она скоро появится. Можете открыть эту страницу на других языках.",
-    'buttons': [
-        {'label': 'Английская версия', 'link': '/en/stat.html'},
-        {'label': 'Польская версия', 'link': '/pl/stat.html'},
-    ],
-}
+html_title = 'Глобальная статистика'
+cards_label = html_title
+card_height = 210
+cards = (
+    {'title': '50% пригодных для жизни земель в мире используется для сельского хозяйства',
+     'subtitle': 'winstein.org',
+     'link': 'https://www.winstein.org/publ/1-1-0-5665',
+     'preview': 'https://sun9-49.userapi.com/MB6LSlfMfslqmnnCdL2AFfAWt8y70DUOzBBLdw/zCpwkcI1Mf8.jpg',
+     'description': "На протяжении большей части человеческой истории большая часть земли в мире была дикой: леса, луга и кустарники доминировали в ее ландшафтах. За последние несколько столетий ситуация резко изменилась: дикие места обитания были вытеснены, превратив их в сельскохозяйственные угодья.",
+    },
+    {'title': 'Опосредованные углеродные выбросы мясоедов превзошли выбросы веганов в три раза',
+     'subtitle': 'N+1',
+     'link': 'https://nplus1.ru/news/2023/08/01/vegs-vs-meat-eaters',
+     'preview': 'https://minio.nplus1.ru/app-images/815142/64c79667d8518_img_desktop.jpg',
+     'description': "Люди, употребляющие в пищу большое количество мясных продуктов (включая рыбу и мясо птицы), производят в три раза больше опосредованных антропогенных выбросов углекислого газа и в 15 раз больше выбросов метана, чем веганы.",
+    },
+    {'title': 'Половина пластика в океане — с рыболовных судов — ООН',
+     'subtitle': 'МедиаПалуба',
+     'link': 'https://paluba.media/news/32322',
+     'preview': '../templates/assets/fishing_boat.jpg',
+     'description': "Половина всего пластикового мусора в Мировом океане попала туда с 4,5 миллионов рыболовных судов, ведущих промысел в международных водах и в территориальных водах стран.",
+    },
+    {'title': 'Доклад: Amnesty International возложила ответственность за дефорестацию Амазонии на промышленное животноводство',
+     'subtitle': 'Amnesty International',
+     'link': 'https://eurasia.amnesty.org/2019/11/26/amnesty-international-vozlozhila-otvetstvennost-za-deforestacziyu-amazonii-na-promyshlennoe-zhivotnovodstvo/',
+     'preview': '../templates/assets/amazon.jpg',
+     'description': "Amnesty International в своём новом докладе пришла к выводу, что промышленное животноводство является основной движущей силой незаконного захвата земель в заповедниках и на территориях коренных народов в бразильской Амазонии.",
+    },
+    {'title': 'Пластик в океане',
+     'subtitle': 'The Ocean Foundation',
+     'link': 'https://oceanfdn.org/ru/plastic-in-the-ocean/',
+     'preview': '../templates/assets/plastic_cup.jpg',
+     'description': "Пластик, наиболее распространенная форма стойкого морского мусора, является одной из самых насущных проблем морских экосистем.",
+    },
+)
